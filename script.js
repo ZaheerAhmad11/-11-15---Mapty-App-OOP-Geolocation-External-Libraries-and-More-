6013807da96170b716f390cab81202cbb191de98
Zaheer Ahmad <raozaheer626@gmail.com>
2026-04-11 12:34:13 +0500

    1 to 10

--- script.js
+++ script.js
@@ -10,3 +10,239 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// -----------------------01 Section intro
+// -----------------------03 Project Overview
+
+// -----------------------04
+// -------------------------How to Plan a Web Project
+//  1) User Stories
+//  2) Features
+//  3) FlowChart
+//  4) Architecture
+
+/* 
+  1) User Stories..
+Description of the Application functionality from the user perspective
+  */
+
+// -----------------------05
+// -------------------------Using The Geolocation Api
+// if (navigator.geolocation)
+//     navigator.geolocation.getCurrentPosition(
+// function (position) {
+//     const {latitude} = position.coords;
+//     const {longitude} = position.coords;
+//     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+// },
+// function () {
+//     alert('Could not get your Location');
+// },
+// );
+
+// -----------------------06
+// -------------------------Displaying a map using 'Leaflet' Library
+// if (navigator.geolocation)
+//   navigator.geolocation.getCurrentPosition(
+//     function (position) {
+//       const { latitude } = position.coords;
+//       const { longitude } = position.coords;
+//       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+//       const coords = [latitude, longitude];
+//       const map = L.map('map').setView(coords, 15);
+
+//       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+//         attribution:
+//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//       }).addTo(map);
+
+//       L.marker(coords)
+//         .addTo(map)
+//         .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//         .openPopup();
+//     },
+//     function () {
+//       alert('Could not get your Location');
+//     },
+//   );
+
+// -----------------------07
+// -------------------------Displaying a Map Marker
+// if (navigator.geolocation)
+//     navigator.geolocation.getCurrentPosition(
+// function (position) {
+//       const { latitude } = position.coords;
+//       const { longitude } = position.coords;
+//       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+//       const coords = [latitude, longitude];
+//       const map = L.map('map').setView(coords, 15);
+
+//       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+//           attribution:
+//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//         }).addTo(map);
+
+//         map.on('click', function (mapEvent) {
+//             console.log(mapEvent);
+//             const {lat, lng} = mapEvent.latlng;
+//         L.marker([lat, lng])
+//         .addTo(map)
+//         .bindPopup(L.popup({
+//             maxWidth:250,
+//             minWidth: 100,
+//             autoClose: false,
+//             closeOnClick: false,
+//             className: 'running-popup'
+//         }
+//     ))
+//     .setPopupContent('WorkOut')
+//         .openPopup();
+//         });
+//     },
+//     function () {
+//       alert('Could not get your Location');
+//     },
+// );
+
+// -----------------------08
+// -------------------------Displaying Workout Input Form
+
+// let map, mapEvent;
+
+// if (navigator.geolocation)
+//   navigator.geolocation.getCurrentPosition(
+//     function (position) {
+//       const { latitude } = position.coords;
+//       const { longitude } = position.coords;
+//       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+//       const coords = [latitude, longitude];
+//       map = L.map('map').setView(coords, 15);
+
+//       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+//         attribution:
+//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//       }).addTo(map);
+//       //Handling Click on map
+//       map.on('click', function (mapE) {
+//         mapEvent = mapE;
+//         form.classList.remove('hidden');
+//         inputDistance.focus();
+//       });
+//     },
+//     function () {
+//       alert('Could not get your Location');
+//     },
+//   );
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   inputDistance.value =
+//     inputElevation.value =
+//     inputDuration.value =
+//     inputCadence.value =
+//       '';
+//   console.log(mapEvent);
+//   //Display Marker
+//   const { lat, lng } = mapEvent.latlng;
+//   L.marker([lat, lng])
+//     .addTo(map)
+//     .bindPopup(
+//       L.popup({
+//         maxWidth: 250,
+//         minWidth: 100,
+//         autoClose: false,
+//         closeOnClick: false,
+//         className: 'running-popup',
+//       }),
+//     )
+//     .setPopupContent('WorkOut')
+//     .openPopup();
+// });
+
+// inputType.addEventListener('change', function () {
+//   inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+//   inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+// });
+
+// -----------------------09
+// -------------------------Project Architecture
+let map, mapEvent;
+class App {
+  #map;
+  #mapEvent;
+  
+  constructor() {
+    this._getPosition();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your Position');
+        },
+      );
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+    this.#map = L.map('map').setView(coords, 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+    //Handling Click on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+    inputDistance.value =
+      inputElevation.value =
+      inputDuration.value =
+      inputCadence.value =
+        '';
+    //Display Marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        }),
+      )
+      .setPopupContent('WorkOut')
+      .openPopup();
+  }
+}
+
+//----
+const app = new App();
